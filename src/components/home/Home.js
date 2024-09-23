518712e09e1392347c5da1df57e6f5da7826d7ac
vexior27 <jakubmalec07@gmail.com>
2024-09-23 20:38:28 +0200

    created qucik access & monthly summary containers

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
             <h3 className='text-3xl font-poppinsMedium'>My budget</h3>
             <h1 className='text-5xl font-poppinsBold'>535,68 $</h1>
          </div>
-         <div className='flex items-start justify-start p-5 gap-5'>
+         <div className='grid grid-cols-2 grid-rows-2'>
             <div className='w-2/5 gap-5 p-5 flex items-start justify-start flex-col'>
                <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Recent expenses</h1>
                <div className='bg-gray-50 rounded-lg w-full h-2/3  overflow-scroll flex items-start justify-start flex-col '>
@@ -20,7 +20,16 @@ export default function Home() {
                </div>
             </div>
             <div className='w-3/5 gap-5 p-5 flex items-start justify-start flex-col'>
-               <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Qucik access</h1>
+               <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Quick access</h1>
+               <div className='w-full bg-green-300 rounded-lg h-1/2 p-5'>
+                  s
+               </div>
+            </div>
+            <div className='w-3/5 gap-5 p-5 flex items-start justify-start flex-col'>
+               <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Monthly summary</h1>
+               <div className='w-full bg-green-300 rounded-lg h-1/2 p-5'>
+                  s
+               </div>
             </div>
          </div>
       </div>
