672936dd2d318be3920aee9c08987087513b3707
vexior27 <jakubmalec07@gmail.com>
2024-09-29 18:16:19 +0200

    fixed home display

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,30 +4,32 @@ import ReactDOM from 'react-dom/client';
 
 import RecentExpensesHeader from './recent-expenses/RecentExpensesHeaders';
 
+import "./style.scss"
+
 export default function Home() {
    return (
-      <div className='w-4/5 h-90vh bg-white ml-auto mr-auto rounded-xl top-1/2 overflow-hidden'>
+      <div className='w-4/5 h-90vh bg-white ml-auto mr-auto overflow-hidden rounded-xl '>
          <div className='w-full h-1/4 bg-green-400 flex items-start justify-end flex-col gap-2 p-10 pl-15 text-white'>
             <h3 className='text-3xl font-poppinsMedium'>My budget</h3>
             <h1 className='text-5xl font-poppinsBold'>535,68 $</h1>
          </div>
-         <div className='grid grid-cols-2 grid-rows-2'>
-            <div className='w-2/5 gap-5 p-5 flex items-start justify-start flex-col'>
+         <div className='container h-3/4'>
+            <div className='recent gap-5 p-5 flex items-start justify-start flex-col'>
                <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Recent expenses</h1>
-               <div className='bg-gray-50 rounded-lg w-full h-2/3  overflow-scroll flex items-start justify-start flex-col '>
+               <div className='bg-gray-50 rounded-lg w-full  overflow-y-scroll flex items-start justify-start flex-col '>
                < RecentExpensesHeader day="today" />
                < RecentExpensesHeader day="yesterday" />
                </div>
             </div>
-            <div className='w-3/5 gap-5 p-5 flex items-start justify-start flex-col'>
+            <div className='quick gap-5 p-5 flex items-start justify-start flex-col'>
                <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Quick access</h1>
-               <div className='w-full bg-green-300 rounded-lg h-1/2 p-5'>
+               <div className='w-full bg-gray-50 rounded-lg p-5 h-full'>
                   s
                </div>
             </div>
-            <div className='w-3/5 gap-5 p-5 flex items-start justify-start flex-col'>
+            <div className='summary gap-5 p-5 flex items-start justify-start flex-col'>
                <h1 className='text-gray-600 font-poppinsSemiBold text-xl'>Monthly summary</h1>
-               <div className='w-full bg-green-300 rounded-lg h-1/2 p-5'>
+               <div className='w-full bg-gray-50 rounded-lg p-5 h-full'>
                   s
                </div>
             </div>
--- src/components/home/recent-expenses/RecentExpenses.js
+++ src/components/home/recent-expenses/RecentExpenses.js
@@ -5,7 +5,7 @@ import { IoIosArrowForward } from "react-icons/io";
 
 export default function RecentExpenses(props) {
    return (
-      <div className="container bg-gray-100 rounded-md w-full h-20 hover:cursor-pointer hover:bg-gray-200 flex items-start justify-center overflow-hidden duration-150 ease-in text-gray-600">
+      <div className="expenseContainer bg-gray-100 rounded-md w-full h-20 hover:cursor-pointer hover:bg-gray-200 flex items-start justify-center overflow-hidden duration-150 ease-in text-gray-600">
          <div className="imageContainer w-1/6  h-full flex items-center justify-center">
             <img src={props.imgSrc} alt="img"
             className="w-2/3"/>
